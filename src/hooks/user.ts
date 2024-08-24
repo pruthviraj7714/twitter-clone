a9b5a132ad1e1a8811c53ca419263b1d28fdccc9
pruthviarj7714 <marganepruthviraj@gmail.com>
2024-08-24 23:36:37 +0530

    added userinfo hook

---
+++ src/hooks/user.ts
@@ -0,0 +1,31 @@
+"use client"
+import { useToast } from "@/components/ui/use-toast";
+import axios from "axios";
+import { useEffect, useState } from "react"
+
+export const useUserInfo = () => {
+    const [userInfo, setUserInfo] = useState<any>({});
+    const [isLoading, setIsLoading] = useState(true);
+    const { toast } = useToast();
+
+    const getUserInfo = async () => {
+        try {
+            const res = await axios.get('/api/user/info');
+            setUserInfo(res.data);
+            console.log(res.data);
+        } catch (error : any) {
+            toast({
+                title : error.response.data.message
+            })
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        getUserInfo();
+    }, [])
+
+    return {isLoading, userInfo };
+
+}

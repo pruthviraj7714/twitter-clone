38ea17c54939fdfa05eee56967f3dd8ba259212a
pruthviarj7714 <marganepruthviraj@gmail.com>
2024-08-25 21:44:34 +0530

    added basic right sidebar component

---
+++ src/components/RightSidebar.tsx
@@ -0,0 +1,10 @@
+export default function RightSidebar() {
+    return (
+        <div className="flex flex-col justify-center items-center">
+            <div className="flex flex-col justify-center items-center border border-white/15 rounded-xl h-20">
+                <h1 className="font-bold text-xl">Who to follow</h1>
+                
+            </div>
+        </div>
+    )
+}

5a5b93207a1503422d0009a87e0af440d0bf434e
pruthviarj7714 <marganepruthviraj@gmail.com>
2024-08-31 17:07:18 +0530

    small fix

--- src/app/api/user/all/route.ts
+++ src/app/api/user/all/route.ts
@@ -3,6 +3,8 @@ import prisma from "@/lib/db";
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 
+export const dynamic = 'force-dynamic';
+
 export async function GET(req: NextRequest) {
   try {
     const session = await getServerSession(authOptions);
